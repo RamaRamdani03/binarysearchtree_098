6fdcdff14dca58891205418143af0e8382ae46e5
RamaRamdani03 <ramaramdani449@gamil.com>
2023-07-02 16:55:45 +0700

    membuat anak kiri dan kanan dari node baru menunjuk ke poin null

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -32,3 +32,5 @@ public:
 	{
 		Node* newNode = new Node(element, NULL, NULL);	// Alocate memory for the new node
 		newNode->info = element;	// Assign value to the data field of the new node
+		newNode->leftchild = NULL;	// Make the left child of the new node point to NULL
+		newNode->rightchild = NULL;	// Make the right child of the new node point to NULL
