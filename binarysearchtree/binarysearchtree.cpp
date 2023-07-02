878b5d7174b2c780d0c09c79cb992445a2e78727
RamaRamdani03 <ramaramdani449@gamil.com>
2023-07-02 16:54:08 +0700

    menetapkan nilai pada data field dari node baru

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -31,3 +31,4 @@ public:
 	void insert(string element)	// Insert a node in the binary search tree
 	{
 		Node* newNode = new Node(element, NULL, NULL);	// Alocate memory for the new node
+		newNode->info = element;	// Assign value to the data field of the new node
